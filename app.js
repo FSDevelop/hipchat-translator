6f71aa5e9651342df0ee442241fcb32b5a2dfcca
EC2 Default User <ec2-user@ip-172-31-52-45.ec2.internal>
2016-05-20 18:45:06 +0000

    Some changes

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 var express = require('express');
 var http = require('http');
 var app = express();
-
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var curl = require('node-curl');
+  /*curl('www.google.com', function(err) {
+    console.info(this.status);
+    console.info('-----');
+    console.info(this.body);
+    console.info('-----');
+    console.info(this.info('SIZE_DOWNLOAD'));
+  });*/
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.listen(3002, function() {
+app.listen(3000, function() {
     console.log('Running Hipchat Translator...');
 });
 
-app.post('/translate', function(req, res, err) {
-    var requestMessage = req.body.item.message.message;
-    var command = calculation.split('/translate ');
+app.post('*', function(req, res, err) { 
+//    console.log('request coming from hipchat');
+    var message = req.body.item.message.message;
+    var command = message.split('/translate ');
     var textToTranslate = command[1];
 
     var API_KEY = "AIzaSyAPD50jtH10mVJbLx070S7hYboy2m3MF9U";
-
+	console.log(textToTranslate);
+    /*
     var APIReq = http.request({
         host: 'googleapis.com',
         port: '80',
@@ -34,5 +43,5 @@ app.post('/translate', function(req, res, err) {
             "notify": false,
             "message_format": "text"
         });
-    });
+    });*/
 });
