3e747822e097c71ff738c2e60f035e66e270300d
Federico Sosa <federico.sosa@modelit.xyz>
2016-05-20 15:48:43 -0300

    Some adjustments

--- app.js
+++ app.js
@@ -3,39 +3,34 @@ var http = require('http');
 var app = express();
 var bodyParser = require('body-parser');
 var curl = require('node-curl');
-  /*curl('www.google.com', function(err) {
-    console.info(this.status);
-    console.info('-----');
-    console.info(this.body);
-    console.info('-----');
-    console.info(this.info('SIZE_DOWNLOAD'));
-  });*/
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
 app.listen(3000, function() {
-    console.log('Running Hipchat Translator...');
+    console.log('Running Hipchat Translator on port 3000...');
 });
 
-app.post('*', function(req, res, err) { 
-//    console.log('request coming from hipchat');
+app.post('*', function(req, res, err) {
+    console.log('There is a request from HipChat');
+    
     var message = req.body.item.message.message;
     var command = message.split('/translate ');
     var textToTranslate = command[1];
 
     var API_KEY = "AIzaSyAPD50jtH10mVJbLx070S7hYboy2m3MF9U";
-	console.log(textToTranslate);
-    /*
-    var APIReq = http.request({
-        host: 'googleapis.com',
-        port: '80',
-        path: '/language/translate/v2?q=' + textToTranslate + '&target=es&key=' + API_KEY,
-        method: 'GET'
-    }, function(APIRes) {
-        console.log(APIRes);
-        APIReq.end();
+    console.log(textToTranslate);
+
+
+    curl('https://googleapis.com/language/translate/v2?q=' + textToTranslate + '&target=es&key=' + API_KEY, function(err) {
+        console.log('Curl made.');
+        console.info(this.status);
+        console.info('-----');
+        console.info(this.body);
+        console.info('-----');
+        console.info(this.info('SIZE_DOWNLOAD'));
 
         res.send({
             "color": "green",
@@ -43,5 +38,5 @@ app.post('*', function(req, res, err) {
             "notify": false,
             "message_format": "text"
         });
-    });*/
+    });
 });
