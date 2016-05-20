6baec032be39c6694f76140ca862da0737704a23
Federico Sosa <federico.sosa@modelit.xyz>
2016-05-20 16:00:20 -0300

    Trying google-translate package

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var http = require('http');
 var app = express();
 var bodyParser = require('body-parser');
+var apiKey = "AIzaSyAPD50jtH10mVJbLx070S7hYboy2m3MF9U";
+var googleTranslate = require('google-translate')(apiKey);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -14,27 +16,17 @@ app.listen(3000, function() {
 
 app.post('*', function(req, res, err) {
     console.log('There is a request from HipChat');
-    
+
     var message = req.body.item.message.message;
     var command = message.split('/translate ');
     var textToTranslate = command[1];
-
-    var API_KEY = "AIzaSyAPD50jtH10mVJbLx070S7hYboy2m3MF9U";
-    console.log(textToTranslate);
     
-    var options = {
-      host: 'googleapis.com',
-      port: 443,
-      path: '/language/translate/v2?q=' + textToTranslate + '&target=es&key=' + API_KEY,
-      method: 'GET'
-    };
-
-    var reqAPI = http.request(options, function(resAPI) {
-      console.log('STATUS: ' + resAPI.statusCode);
-      console.log('HEADERS: ' + JSON.stringify(resAPI.headers));
-      resAPI.setEncoding('utf8');
-      resAPI.on('data', function (chunk) {
-        console.log('BODY: ' + chunk);
-      });
+    googleTranslate.translate(textToTranslate, 'es', function(err, translation) {
+        res.send({
+            "color": "green",
+            "message": "Translation: " + translation.translatedText,
+            "notify": false,
+            "message_format": "text"
+        });
     });
 });
